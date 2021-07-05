3ade010de29ed4b5a41c0ca7378355ec7de00e31
10nymph <xyl13129751617@163.com>
2021-07-05 20:02:16 +0800

    全局对象和模块化开发（一）

---
+++ 01_LEARN-NODE/02_给Node传递参数/02_Node程序的输出.js
@@ -0,0 +1,19 @@
+console.log(process.argv[2]);
+console.log(process.argv[3]);
+
+console.clear();
+process.argv.forEach(item =>{
+    console.log(item);
+})
+
+console.trace();
+
+function foo(){
+    bar();
+}
+
+function bar() {
+    console.trace();
+}
+
+foo();
